2f54d2999c539e9aa11252e14f5f69cf388de927
Felipe Mantovani <marcelus20felipe@gmail.com>
2021-12-03 17:53:23 +0000

    service atm runing springboot web service successfully working.

---
+++ atm/src/test/java/atm/machine/atm/AtmApplicationTests.java
@@ -0,0 +1,13 @@
+package atm.machine.atm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
